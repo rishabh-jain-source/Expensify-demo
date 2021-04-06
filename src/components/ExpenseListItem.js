a997d26e8e309c3454a4a454d6cc56f62d31434b
rishabh jain <rishabh.jain@sourcesoftsolutions.com>
2021-04-06 12:42:01 +0530

    workflow

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,12 +3,17 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { removeExpense } from '../actions/expenses'
 import EditExpense from './EditExpense'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 export const ExpenseListItem = ({ id,dispatch,description, amount, createdAt }) => (
     <div>
         <Link to={`/EditExpense/${id}`}>{description}</Link>
-        <p>{amount}-{createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,00.00')}
+        -
+        {moment(createdAt).format('MMMM Do,YYYY')}</p>
         {/* <button onClick={() => {
             dispatch(removeExpense({id}))
         }}>Remove</button> */}
